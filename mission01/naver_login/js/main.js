c2832321aaa8fbbad5dafe881de7d7b7941cf0e5
BELLORI <bellori729@gmail.com>
2023-12-08 21:52:58 +0900

    1. 아이디(이메일) 입력란 체크 기능 추가 2. 비밀번호 입력란 체크 기능 추가 3. 로그인 버튼 클릭 시 user.id의 값과 input의 값을 비교 및 user.pw의 값과 input의 값을 비교 추가

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,8 +1,7 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
 /*
 
@@ -13,24 +12,61 @@ const user = {
 
 */
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// 규식이행님
 
-  return re.test(String(text).toLowerCase())
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(text).toLowerCase());
 }
 
-function pwReg(text){
+function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
+// 아이디(이메일) 입력란 체크
 
+const userEmailInput = document.querySelector("#userEmail");
 
+function HandleIdInput(e) {
+  e.preventDefault();
+  if (emailReg(userEmailInput.value)) {
+    userEmailInput.classList.remove("is--invalid");
+  } else {
+    userEmailInput.classList.add("is--invalid");
+  }
+}
 
+userEmailInput.addEventListener("input", HandleIdInput);
 
+// 비밀번호 입력란 체크
 
+const userPasswordInput = document.querySelector("#userPassword");
 
+function HandlePwInput(e) {
+  e.preventDefault();
+  if (pwReg(userPasswordInput.value)) {
+    userPasswordInput.classList.remove("is--invalid");
+  } else {
+    userPasswordInput.classList.add("is--invalid");
+  }
+}
 
+userPasswordInput.addEventListener("input", HandlePwInput);
 
+// 로그인 버튼 클릭 시 user.id의 값과 input의 값을 비교 및 user.pw의 값과 input의 값을 비교
 
+const loginButton = document.querySelector(".btn-login");
 
+function handleLogin(e) {
+  e.preventDefault();
+  if (userEmailInput.value === user.id && userPasswordInput.value === user.pw) {
+    window.location.href = "welcome.html";
+  } else {
+    loginButton.classList.add("is--invalid");
+  }
+}
+
+loginButton.addEventListener("click", handleLogin);
