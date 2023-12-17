d2f78bc8f90326a21f1ad4fbb7568cad104d696b
BELLORI <bellori729@gmail.com>
2023-12-17 19:08:57 +0900

    변수 설정 / 배경 색상, 이미지, 텍스트, "is-active" 클래스 변경 함수 추가 / 클릭 이벤트 추가

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -1,4 +1,3 @@
-
 /* 
 
 1. 클릭 이벤트 활성화
@@ -9,16 +8,48 @@
 
 */
 
+// 변수 설정
+const navigation = getNode(".navList");
+const visualImage = getNode(".visual img");
+const nickName = getNode(".nickName");
 
+// nav 클릭 시 배경 색상 변경
+function setBgColor(colorA, colorB = "#000") {
+  setCss(
+    "body",
+    "background",
+    `linear-gradient(to bottom, ${colorA}, ${colorB})`
+  );
+}
 
+// 이미지 변경
+function setImage(index) {
+  visualImage.src = `./assets/${data[index - 1].name}.jpeg`;
+  visualImage.alt = data[index - 1].alt;
+}
 
+// 텍스트 변경
+function setNameText(index) {
+  nickName.textContent = data[index - 1].name;
+}
 
+// "is-active" 변경
+function setActiveClass(li) {
+  const list = Array.from(navigation.children);
+  list.forEach((li) => removeClass(li, "is-active"));
+  addClass(li, "is-active");
+}
 
+// 클릭 이벤트
+function handleClick(e) {
+  const li = e.target.closest("li");
+  if (!li) return;
 
+  const index = li.dataset.index;
+  setActiveClass(li);
+  setBgColor(data[index - 1].color[0], data[index - 1].color[1]);
+  setImage(index);
+  setNameText(index);
+}
 
-
-
-
-
-
-
+navigation.addEventListener("click", handleClick);
